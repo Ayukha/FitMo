cea8a0d72e187873f19a135be8aa3e27d2625c6d
ayukha <ayussh.khare@gmail.com>
2018-11-18 16:49:22 +0530

    account models added

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,30 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
+
+
+class Profile(models.Model):
+    """
+    Model to store profile of a user
+    """
+    user = models.OneToOneField(User)
+    xp = models.IntegerField(default=0, blank=False, null=True)
+    level = models.IntegerField(max_length=99)
+    location = models.IntegerField(default=0)
+
+    def __str__(self):
+        return '{}'.format(self.user)
+
+    class Meta:
+        app_label = 'accounts'
+        db_table = 'user_profile'
+
+
+# @receiver(post_save, sender=User)
+# def create_or_update_user_profile(sender, instance, created, **kwargs):
+#     if created:
+#         Profile.objects.create(user=instance)
+#     instance.profile.save()
--- accounts/views.py
+++ accounts/views.py
@@ -2,5 +2,12 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import Profile
+from django.contrib.auth.models import User
 
 # Create your views here.
+@receiver(post_save, sender=User)
+def addProfile():
+	pass
