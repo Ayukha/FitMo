468ef4e3613cfd410b5e6569f38c9b0cbb89e3ad
ayukha <ayussh.khare@gmail.com>
2018-11-18 20:16:41 +0530

    urls added

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,9 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
+    url(r'^api-auth/',
+        include('rest_framework.urls',
+                namespace='rest_framework'))
     url(r'^updateprofile/', views.
         ),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from rest_framework.decorators import (
+    									api_view,
+    									permission_classes,
+    								)
 from django.shortcuts import render
+from .serializers import (PlacesSerializer, reviewSerializer)
 
 # Create your views here.
+
+
+def discover(request):
+
+
+def together():
+
+
+def followrequest
+
+
+def confirmfollowrequest
